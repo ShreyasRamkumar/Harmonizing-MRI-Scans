7adb0b7543754b65f9f4f7de69f8f48a12b0f4d6
ShreyasRamkumar <shreyace12@gmail.com>
2023-08-16 17:00:32 -0400

    modified:   nipype_preprocess.py

--- nipype_preprocess.py
+++ nipype_preprocess.py
@@ -12,16 +12,22 @@ def minMaxNormalization(image):
     normalized_image = (image - tensor_min) / (tensor_max - tensor_min)
     return normalized_image
 
-def wf(input_image):
+def image_to_tensor(input_image):
     img = nib.load(input_image)
     img_array = img.get_fdata()
     img_tensor = torch.tensor(img_array, dtype=torch.float32)
+    return img_tensor
+
+def tensor_to_image(input_tensor, output_image_path):
+    image_array = input_tensor.numpy()
+    image = nib.Nifti1Image(image_array, affine=np.eye(4))
+    nib.save(image, output_image_path)
+
+def wf(input_image):
+    img_tensor = image_to_tensor(input_image)
     normalize = Node(Function(input_names=["image"], output_names=["normalized_image"], function=minMaxNormalization), name="min_max_norm")
     normalize.inputs.image=img_tensor
     normalize.run()
-    normalized_image_array = normalize.outputs.normalized_image.numpy()
-    normalized_image = nib.Nifti1Image(normalized_image_array, affine=np.eye(4))
-    nib.save(normalized_image, "./preprocessed1.nii")
-
+    tensor_to_image(normalize.outputs.normalized_image, "preprocessed1.nii")
 
 wf("raw.nii")
