8c17093ae76bd51ebaeb29ff7b464caa6dc6e5d1
Shreyas Ramkumar <101288239+ShreyasRamkumar@users.noreply.github.com>
2024-02-09 00:22:43 -0500

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -12,17 +12,26 @@ import nipype
 # test for github push
 
 class Preprocessing:
+    """A class for preprocessing MRI scans.
 
+    Attributes:
+        input_folder (str): The folder containing the scans to preprocess.
+        output_folder (str): The folder where preprocessed scans will be stored.
+    """
     def __init__(self, input_folder, output_folder):
         """
-        @input_folder: the folder that has the scans you want to preprocess
-        @output_folder: the folder that the preprocessed scans will be stored in 
+        Initializes the Preprocessing class with input and output folders.
+
+        Args:
+            input_folder (str): The folder containing the scans to preprocess.
+            output_folder (str): The folder where preprocessed scans will be stored.
         """
         self.input_folder = input_folder
         self.output_folder = output_folder
         
 
     def run_preprocess(self):
+        """Runs the preprocessing steps for all scans in the input folder."""
         for i in tqdm(self.files):
             self.correctBias(input_image_path=f"./{self.input_folder}/{i}")
             self.extractBrain(i)
@@ -30,7 +39,18 @@ class Preprocessing:
         print("Preprocessing Complete!")
 
     def correctBias(self, input_image_path=None, image_created = False, image = None, image_mask = None):
+        """
+        Corrects bias in the MRI scan.
 
+        Args:
+            input_image_path (str): Path to the input MRI image.
+            image_created (bool): Indicates if the image has already been created.
+            image: The image object.
+            image_mask: The mask image object.
+
+        Returns:
+            sitk.Image: The bias-corrected MRI image.
+        """
         input_image = image
 
         if image_created == False:
@@ -69,6 +89,16 @@ class Preprocessing:
             print("Bias corrected!")
 
     def createMRIMask(self, input_image_path, image_created = False):
+        """
+        Creates a mask for the MRI scan.
+
+        Args:
+            input_image_path (str): Path to the input MRI image.
+            image_created (bool): Indicates if the image has already been created.
+
+        Returns:
+            None
+        """
         img = nib.load(input_image_path)
         data = np.squeeze(img.get_fdata())
         b0_mask, mask = median_otsu(data, median_radius=2, numpass=1)
@@ -77,6 +107,15 @@ class Preprocessing:
         print("Mask created!")
 
     def extractBrain(self, input_image_path):
+        """
+        Performms brain extraction on the MRI scan.
+
+        Args:
+            input_image_path (str): Path to the input MRI image.
+
+        Returns:
+            None
+        """
         image = nib.load("./bixed.nii")
         stripped = robex.robex_stripped(image)
         output_path = input_image_path[:-4]
@@ -84,33 +123,30 @@ class Preprocessing:
         os.remove("./bixed.nii")
 
     def minMaxNormalization(self, image):
+        """
+        Performs min-max normalization on the image.
+
+        Args:
+            image: The input image.
+
+        Returns:
+            torch.Tensor: The normalized image.
+        """
         tensor_min = torch.min(image)
         tensor_max = torch.max(image)
         normalized_image = (image - tensor_min) / (tensor_max - tensor_min)
         return normalized_image
-    
-    def baselineImprov(self):
-        denoise = sitk.MinMaxCurvatureFlowImageFilter()
-        for i in tqdm(os.listdir(self.input_folder)):
-            image_path = os.listdir(f"{self.input_folder}/{i}/anat")
-            image = sitk.ReadImage(f"{self.input_folder}/{i}/anat/{image_path}", sitk.sitkFloat32)
-            image_mask = self.createMRIMask(image_path, image_created=True)
-            bias_corrected_image = self.correctBias(input_image_path=None, image_created=True, image=image, image_mask=image_mask)
-            denoised_image = denoise.Execute(bias_corrected_image)
-            print("Image denoised!")
-            image_array = sitk.GetArrayFromImage(denoised_image)
-            image_tensor = torch.tensor(image_array, dtype=torch.float32)
-            normalized_image = self.minMaxNormalization(image_tensor)
-            print("Image normalized!")
-            norm_img_array = np.array(normalized_image)
-            norm_img_nifti = nib.Nifti1Image(norm_img_array, affine=np.eye(4))
-            nib.save(norm_img_nifti, f"{self.output_folder}/{i}")
-            print("Enhancement Successful!")
 
-class MRITransform:
-    def __init__(self, input_folder_path, output_folder_path) -> None:
-        self.input_folder_path = input_folder_path
-        self.output_folder_path = output_folder_path
+class DatasetPreparation(Preprocessing):
+    """
+    Prepares training and testing datasets using methods from Preprocessing class
+
+    Attributes:
+        input_folder (str): The folder containing the scans to preprocess.
+        output_folder (str): The folder where preprocessed scans will be stored.
+    """
+    def __init__(self, input_folder_path, training_folder_path, ideal_folder_path) -> None:
+        super().__init__(input_folder_path, output_folder_path)
         self.input_files = os.listdir(input_folder_path)
     
     def create_training_data(self):
@@ -121,5 +157,23 @@ class MRITransform:
             image = tio.ScalarImage(f"{self.input_folder_path}/{i}")
             augmented_image = transforms(image)
             augmented_image.save(f"{self.output_folder_path}/{i}")
+    
+    def baselineImprov(self):
+        denoise = sitk.MinMaxCurvatureFlowImageFilter()
+        for i in tqdm(self.input_files):
+            image_path = os.listdir(f"{self.input_folder}/{i}/anat")
+            image = sitk.ReadImage(f"{self.input_folder}/{i}/anat/{image_path}", sitk.sitkFloat32)
+            image_mask = super().createMRIMask(image_path, image_created=True)
+            bias_corrected_image = super().correctBias(input_image_path=None, image_created=True, image=image, image_mask=image_mask)
+            denoised_image = denoise.Execute(bias_corrected_image)
+            print("Image denoised!")
+            image_array = sitk.GetArrayFromImage(denoised_image)
+            image_tensor = torch.tensor(image_array, dtype=torch.float32)
+            normalized_image = super().minMaxNormalization(image_tensor)
+            print("Image normalized!")
+            norm_img_array = np.array(normalized_image)
+            norm_img_nifti = nib.Nifti1Image(norm_img_array, affine=np.eye(4))
+            nib.save(norm_img_nifti, f"{self.output_folder}/{i}")
+            print("Enhancement Successful!")
 
 
